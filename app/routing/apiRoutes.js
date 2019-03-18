cd34f864f6eeb5267e44b00560398fb815c3e634
Jorge Barrientos <jorgebarrientos0311@gmail.com>
2019-03-18 09:32:00 -0500

    compare logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,28 @@ module.exports = function(app) {
     });
 
     app.post("api/friends", function(req, res){
+        
         friends.push(req.body);
+
+        var minimumDifference = 40;
+
+        for(i=0; i<req.body.scores.length; i++){
+            req.body.scoreas[i] = parseInt(req.body.scores[i]);
+        }
+
+        for(i=0; i<friends.length; i++) {
+            var totalDifference = 0;
+            for(j=0; j<friends[i].scores.length; j++) {
+              var difference = Math.abs(req.body.scores[j] - friends[i].scores[j]);
+              totalDifference += difference;
+            }
+      
+            if(totalDifference < minimumDifference) {
+              bestFriend = i;
+              minimumDifference = totalDifference;
+            }
+        }
+
+        res.json(friends[bestFriend]);
     });
 };
