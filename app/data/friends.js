a46aa02b3c26600a55798773b8ea3f4e6d7b28e5
Jorge Barrientos <jorgebarrientos0311@gmail.com>
2019-03-18 08:02:22 -0500

    home.html

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friendsArr = [
+    {
+        name:"Jorge",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiLl8uN24vhAhWk34MKHXKhCV8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F156992736995230022%2F&psig=AOvVaw0xiwOMiuMZxrD0RavmtzMI&ust=1552999230745505",
+        scores:[]
+    }
+]
